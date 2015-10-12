283190200417e3d6f669ee6495ef0699ade80593
51101594 <nxknt93@gmail.com>
2015-10-12 19:44:52 +0700

    khoi tao

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/shop/admin/');
+define('HTTP_CATALOG', 'http://localhost/shop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/shop/admin/');
+define('HTTPS_CATALOG', 'http://localhost/shop/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/xampp/htdocs/Shop/admin/');
+define('DIR_SYSTEM', 'D:/xampp/htdocs/Shop/system/');
+define('DIR_LANGUAGE', 'D:/xampp/htdocs/Shop/admin/language/');
+define('DIR_TEMPLATE', 'D:/xampp/htdocs/Shop/admin/view/template/');
+define('DIR_CONFIG', 'D:/xampp/htdocs/Shop/system/config/');
+define('DIR_IMAGE', 'D:/xampp/htdocs/Shop/image/');
+define('DIR_CACHE', 'D:/xampp/htdocs/Shop/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/xampp/htdocs/Shop/system/storage/download/');
+define('DIR_LOGS', 'D:/xampp/htdocs/Shop/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/xampp/htdocs/Shop/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/xampp/htdocs/Shop/system/storage/upload/');
+define('DIR_CATALOG', 'D:/xampp/htdocs/Shop/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/shop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/shop/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/xampp/htdocs/Shop/catalog/');
+define('DIR_SYSTEM', 'D:/xampp/htdocs/Shop/system/');
+define('DIR_LANGUAGE', 'D:/xampp/htdocs/Shop/catalog/language/');
+define('DIR_TEMPLATE', 'D:/xampp/htdocs/Shop/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/xampp/htdocs/Shop/system/config/');
+define('DIR_IMAGE', 'D:/xampp/htdocs/Shop/image/');
+define('DIR_CACHE', 'D:/xampp/htdocs/Shop/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/xampp/htdocs/Shop/system/storage/download/');
+define('DIR_LOGS', 'D:/xampp/htdocs/Shop/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/xampp/htdocs/Shop/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/xampp/htdocs/Shop/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
